d345b21ba653bc70f773c33a36a6b48b2c716e34
Keshav <sharma.comkeshav@gmail.com>
2026-02-17 19:37:49 +0530

    Add:Login page and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,8 @@
-import { useState } from "react";
+import {Outlet} from "react-router";
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  const handleClick = async () => {
-    try {
-      const response = await fetch("http://localhost:5000/api/hello");
-      const data = await response.json();
-      setMessage(data.message);
-    } catch (error) {
-      console.error("Error:", error);
-      setMessage("Error connecting to backend");
-    }
-  };
-
   return (
-    <div style={{ padding: "50px", textAlign: "center" }}>
-      <h1>PDF Selling Platform</h1>
-
-      <button
-        onClick={handleClick}
-        style={{
-          padding: "10px 20px",
-          fontSize: "16px",
-          cursor: "pointer",
-          backgroundColor: "#007bff",
-          color: "white",
-          border: "none",
-          borderRadius: "5px",
-        }}
-      >
-        Click Me
-      </button>
-
-      {message && (
-        <h2 style={{ marginTop: "20px", color: "green" }}>{message}</h2>
-      )}
-    </div>
+      <Outlet />
   );
 }
 
---
+++ frontend/src/components/Logo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div className="flex flex-col font-medium p-2 leading-2">
+            <h1 className="text-2xl">
+                RAMDOOT
+            </h1>
+            <p className="text-sm">
+                foundation
+            </p>
+        </div>
+    );
+};
+
+export default Logo;
