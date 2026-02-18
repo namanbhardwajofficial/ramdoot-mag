7c61d51344f1ad23d67de32c4fe6e3cc895b9697
ghuleaniketh <ghuleaniketh@gmail.com>
2026-02-18 16:22:59 +0530

    tailwind setup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,28 +15,9 @@ function App() {
   };
 
   return (
-    <div style={{ padding: "50px", textAlign: "center" }}>
-      <h1>PDF Selling Platform</h1>
-
-      <button
-        onClick={handleClick}
-        style={{
-          padding: "10px 20px",
-          fontSize: "16px",
-          cursor: "pointer",
-          backgroundColor: "#007bff",
-          color: "white",
-          border: "none",
-          borderRadius: "5px",
-        }}
-      >
-        Click Me
-      </button>
-
-      {message && (
-        <h2 style={{ marginTop: "20px", color: "green" }}>{message}</h2>
-      )}
-    </div>
+   <div>
+      <h1>Welcome to Ramdoot Mag!</h1>
+   </div>
   );
 }
 
