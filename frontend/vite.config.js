2adb80b5aadfe0e33b053184145cd9bbc8270fe3
Keshav <sharma.comkeshav@gmail.com>
2026-02-27 21:50:08 +0530

    fix: Vite config typo fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-  '@/components': path.resolve(__dirname, 'src/componets'),
-  '@/lib': path.resolve(__dirname, 'lib')
-    }
+      "@": path.resolve(__dirname, "src"),
+      "@/components": path.resolve(__dirname, "src/components"),
+      "@/lib": path.resolve(__dirname, "src/lib"),
+    },
   },
   server: {
     port: 3000,
